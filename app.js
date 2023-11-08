2aefffde203771ee933329cd7c316259a240a4f3
thenatural86 <philip.smalls86@gmail.com>
2023-11-08 16:07:08 -0300

    os module

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const names = require('./names')
-const sayHi = require('./utils')
-const data = require('./alternative')
-// sayHi(secret)
-require('./mind')
-// sayHi(names.philip)
-// sayHi(names.alice)
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+// method return the system uptime in seconds
+console.log(`The system uptime is ${os.uptime} seconds`)
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freemem: os.freemem(),
+}
+
+console.log(currentOS)
---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative')
+require('./mind')
+sayHi('susan')
+sayHi(names.philip)
+sayHi(names.alice)
