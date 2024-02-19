952e24e133967198a909b79396a9914c3b840ecf
thenatural86 <philip.smalls86@gmail.com>
2024-02-19 11:05:00 -0300

    spin up server

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-const { readFile, writeFile } = require('fs')
+const http = require('http')
 
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  const first = result
-
-  readFile('./content/second.txt', 'utf8', (err, result) => {
-    if (err) {
-      return
-    }
-    const second = result
-    writeFile(
-      './content/result-async.txt',
-      `Here is the result : ${first}, ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log
-          return
-        }
-        console.log(result)
-      }
-    )
-  })
+const server = http.createServer((req, res) => {
+  res.write('Welcome to our homepage')
+  res.end()
 })
+
+server.listen(5000)
