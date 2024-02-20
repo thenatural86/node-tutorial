e271d99fcb3a3af2b431eb8c8eb197f61cd4ab4b
thenatural86 <philip.smalls86@gmail.com>
2024-02-20 13:04:37 -0300

    nodemon dependency

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    return res.end('Welcome to our homepage')
-  }
-  if (req.url === '/about') {
-    return res.end('Here is our short history')
-  }
-  res.end(`
-  <h1>Oopps!</h1>
-  <p>We can't seem to find the page you are looking for</p>
-  <a href="/">Back Home</a>
-  `)
-})
-
-server.listen(5000)
+console.log('hello world')
+console.log('hello people')
