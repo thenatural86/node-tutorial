5673599c7b04413df842deb9443cff2355281d5f
thenatural86 <philip.smalls86@gmail.com>
2024-02-21 17:30:41 -0300

    async refactor

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ const getText = (path) => {
   return new Promise((resolve, reject) => {
     readFile(path, 'utf8', (err, data) => {
       if (err) {
-        console.log(err)
         reject(err)
         return
       } else {
-        console.log(data)
         resolve(data)
       }
     })
   })
 }
 
-getText('./content/first.txt')
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err))
+const start = async () => {
+  try {
+    const first = await getText('./content/first.txt')
+    const second = await getText('./content/second.txt')
+    console.log(first)
+    console.log(second)
+  } catch (error) {
+    console.log(err)
+  }
+}
+
+start()
+
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err))
