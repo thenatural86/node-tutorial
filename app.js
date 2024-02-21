58fcd32288e86e3580ddf2c3295b317af10eeab8
thenatural86 <philip.smalls86@gmail.com>
2024-02-21 15:15:06 -0300

    event loop

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log('hello world')
-console.log('hello people')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log('request event')
+  res.end('Hello World')
+})
+
+server.listen(5000, () => {
+  console.log('Server listening on port : 5000...')
+})
