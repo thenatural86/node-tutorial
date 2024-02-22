eab21cc820f9128a1cdedace23bd9a953e44c901
thenatural86 <philip.smalls86@gmail.com>
2024-02-21 22:44:39 -0300

    promise module

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-const { readFile } = require('fs')
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(err)
-        return
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 const start = async () => {
   try {
-    const first = await getText('./content/first.txt')
-    const second = await getText('./content/second.txt')
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile(
+      './content/result-mind-grenade.txt',
+      `THIS IS GREAT : ${first} ${second}`,
+      { flag: 'a' }
+    )
     console.log(first)
     console.log(second)
   } catch (error) {
@@ -25,6 +20,18 @@ const start = async () => {
 }
 
 start()
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err)
+//         return
+//       } else {
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
 
 // getText('./content/first.txt')
 //   .then((result) => console.log(result))
