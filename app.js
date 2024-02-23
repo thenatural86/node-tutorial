c725af0d1052a7fe855e8a97ef0221dca4b2c912
thenatural86 <philip.smalls86@gmail.com>
2024-02-23 12:40:12 -0300

    event emitter

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
-const { readFile, writeFile } = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const EventEmitter = require('events')
 
-const start = async () => {
-  try {
-    const first = await readFile('./content/first.txt', 'utf8')
-    const second = await readFile('./content/second.txt', 'utf8')
-    await writeFile(
-      './content/result-mind-grenade.txt',
-      `THIS IS GREAT : ${first} ${second}`,
-      { flag: 'a' }
-    )
-    console.log(first)
-    console.log(second)
-  } catch (error) {
-    console.log(err)
-  }
-}
+const customEmitter = new EventEmitter()
 
-start()
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, 'utf8', (err, data) => {
-//       if (err) {
-//         reject(err)
-//         return
-//       } else {
-//         resolve(data)
-//       }
-//     })
-//   })
-// }
+customEmitter.on('response', () => {
+  console.log(`data received`)
+})
 
-// getText('./content/first.txt')
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err))
+customEmitter.emit('response')
